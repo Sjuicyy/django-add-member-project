0b379d188e7a608673cc629b2393b588c9fa3b73
SHYAM JOSHI <sjuicyygmail.com@SAMMYs-MacBook-Air.local>
2022-09-28 12:52:15 +0545

    new

--- members/views.py
+++ members/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse('hello world')
+    template=loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
